3270d3a0c89fcc2ff0c4a1af6fd4b809a79f8660
Saravana Kumar Arumugam <saravanakumar.a.o@gmail.com>
2018-08-14 14:55:28 +0530

    Fixed references

--- EliteGST/Forms/PartyList.cs
+++ EliteGST/Forms/PartyList.cs
@@ -1,16 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 using EliteGST.Data;
-using Elite.Utilities;
 using EliteGST.Data.Models;
 using EliteGST.Data.Repositories;
 using MySql.Data.MySqlClient;
+using Elite.Utilities;
 
 namespace EliteGST.Forms
 {
