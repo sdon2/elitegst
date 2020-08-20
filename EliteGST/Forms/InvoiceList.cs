847c19cbcca1d36a4ab8b9f7c1bd2a95d4bb9b36
Saravanakumar Arumugam <saravanakumar.a.o@gmail.com>
2020-08-20 09:40:41 +0530

    Removed margins

--- EliteGST/Forms/InvoiceList.cs
+++ EliteGST/Forms/InvoiceList.cs
@@ -418,7 +418,7 @@ namespace EliteGST.Forms
                 {
                     using (var pdf = new ReportDocument())
                     {
-                        pdf.Margins = 0.25f;
+                        //pdf.Margins = 0.25f;
                         pdf.PageSize = PageSizes.A4;
                         pdf.PageOrientation = PageOrientations.Portrait;
                         pdf.AddCSS("reports/css/invoice-style.css");
